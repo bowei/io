1108bc7170a828d2362d517b2b71e84e4d2e7221
Bowei Du <bowei.du@instart.co>
2013-04-19 20:23:25 -0700

    Initial import

---
+++ src/io/driver.h
@@ -0,0 +1,14 @@
+#ifndef IO_DRIVER_H_
+#define IO_DRIVER_H_
+
+namespace io {
+
+class Driver {
+public:
+
+private:
+};
+
+} // namespace io
+
+#endif // IO_DRIVER_H_
---
+++ src/io/fiber.h
@@ -0,0 +1,11 @@
+#ifndef IO_FIBER_H_
+#define IO_FIBER_H_
+
+namespace io {
+
+class Fiber {
+};
+
+} // namespace io
+
+#endif // IO_FIBER_H_
